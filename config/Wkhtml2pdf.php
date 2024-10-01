8cce529b01cbaedfecd7d76ad2c24cc7a4bca42b
William Andoh-Mensah <menswill149@gmail.com>
2024-10-01 23:13:06 +0000

    conflicts

--- config/Wkhtml2pdf.php
+++ config/Wkhtml2pdf.php
@@ -3,7 +3,7 @@
 return [
 
     'debug'       => env('APP_DEBUG_PDF', true),
-    'binpath'     => '/opt/bitnami/projects/ticketsystem/vendor/nitmedia/wkhtml2pdf/src/Nitmedia/Wkhtml2pdf/lib/',
+    'binpath'     => 'lib/',
     'binfile'     => env('WKHTML2PDF_BIN_FILE', 'wkhtmltopdf-amd64'),
-    'output_mode' => 'F',
+    'output_mode' => 'I',
 ];
