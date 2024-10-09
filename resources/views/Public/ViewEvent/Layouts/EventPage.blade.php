c7fd62af9e45dad27747c6a3a3444b828a524877
William Andoh-Mensah <menswill149@gmail.com>
2024-10-09 11:54:32 +0000

    transform

--- resources/views/Public/ViewEvent/Layouts/EventPage.blade.php
+++ resources/views/Public/ViewEvent/Layouts/EventPage.blade.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <!--
+    <head>
+        <!--
                   _   _                 _ _
              /\  | | | |               | (_)
             /  \ | |_| |_ ___ _ __   __| |_ _______   ___ ___  _ __ ___
@@ -11,119 +10,94 @@
          /_/    \_\__|\__\___|_| |_|\__,_|_/___\___(_)___\___/|_| |_| |_|
 
         -->
-    <title>{{{$event->title}}} - Attendize.com</title>
+        <title>{{{$event->title}}} - Attendize.com</title>
 
 
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0" />
-    <link rel="canonical" href="{{$event->event_url}}" />
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0" />
+        <link rel="canonical" href="{{$event->event_url}}" />
 
 
-    <!-- Open Graph data -->
-    <meta property="og:title" content="{{{$event->title}}}" />
-    <meta property="og:type" content="article" />
-    <meta property="og:url" content="{{$event->event_url}}?utm_source=fb" />
-    @if($event->images->count())
-    <meta property="og:image" content="{{config('attendize.cdn_url_user_assets').'/'.$event->images->first()['image_path']}}" />
-    @endif
-    <meta property="og:description" content="{{{Str::words(md_to_str($event->description), 20)}}}" />
-    <meta property="og:site_name" content="Attendize.com" />
-    <!--[if lt IE 9]>
+        <!-- Open Graph data -->
+        <meta property="og:title" content="{{{$event->title}}}" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="{{$event->event_url}}?utm_source=fb" />
+        @if($event->images->count())
+        <meta property="og:image" content="{{config('attendize.cdn_url_user_assets').'/'.$event->images->first()['image_path']}}" />
+        @endif
+        <meta property="og:description" content="{{{Str::words(md_to_str($event->description), 20)}}}" />
+        <meta property="og:site_name" content="Attendize.com" />
+        <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
-    @yield('head')
+        @yield('head')
 
-    {!!Html::style(config('attendize.cdn_url_static_assets').'/assets/stylesheet/frontend.css')!!}
+       {!!Html::style(config('attendize.cdn_url_static_assets').'/assets/stylesheet/frontend.css')!!}
 
-    <!--Bootstrap placeholder fix-->
-    <style>
-        ::-webkit-input-placeholder {
-            /* WebKit browsers */
-            color: #ccc !important;
-        }
-
-        :-moz-placeholder {
-            /* Mozilla Firefox 4 to 18 */
-            color: #ccc !important;
-            opacity: 1;
-        }
-
-        ::-moz-placeholder {
-            /* Mozilla Firefox 19+ */
-            color: #ccc !important;
-            opacity: 1;
-        }
-
-        :-ms-input-placeholder {
-            /* Internet Explorer 10+ */
-            color: #ccc !important;
-        }
-
-        input,
-        select {
-            color: #999 !important;
-        }
-
-        .btn {
-            color: #fff !important;
-        }
-    </style>
-    @if ($event->bg_type == 'color' || Request::input('bg_color_preview'))
-    <style>
-        body {
-            background-color: {
-                    {
-                    (Request::input('bg_color_preview') ? '#' .Request::input('bg_color_preview') : $event->bg_color)
-                }
+        <!--Bootstrap placeholder fix-->
+        <style>
+            ::-webkit-input-placeholder { /* WebKit browsers */
+                color:    #ccc !important;
+            }
+            :-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+                color:    #ccc !important;
+                opacity:  1;
+            }
+            ::-moz-placeholder { /* Mozilla Firefox 19+ */
+                color:    #ccc !important;
+                opacity:  1;
+            }
+            :-ms-input-placeholder { /* Internet Explorer 10+ */
+                color:    #ccc !important;
             }
 
-            !important;
-        }
-    </style>
-    @endif
+            input, select {
+                color: #999 !important;
+            }
 
-    @if (($event->bg_type == 'image' || $event->bg_type == 'custom_image' || Request::input('bg_img_preview')) && !Request::input('bg_color_preview'))
-    <style>
-        body {
-            background: url({{(Request::input('bg_img_preview') ? URL: :to(Request::input('bg_img_preview')): asset(config('attendize.cdn_url_static_assets').'/' .$event->bg_image_path))
-        }}) no-repeat center center fixed;
-        background-size: cover;
-        }
-    </style>
-    @endif
+            .btn {
+                color: #fff !important;
+            }
 
-</head>
+        </style>
+        @if ($event->bg_type == 'color' || Request::input('bg_color_preview'))
+            <style>body {background-color: {{(Request::input('bg_color_preview') ? '#'.Request::input('bg_color_preview') : $event->bg_color)}} !important; }</style>
+        @endif
 
-<body class="attendize">
-    <div id="event_page_wrap" vocab="http://schema.org/" typeof="Event">
-        @yield('content')
+        @if (($event->bg_type == 'image' || $event->bg_type == 'custom_image' || Request::input('bg_img_preview')) && !Request::input('bg_color_preview'))
+            <style>
+                body {
+                    background: url({{(Request::input('bg_img_preview') ? URL::to(Request::input('bg_img_preview')) :  asset(config('attendize.cdn_url_static_assets').'/'.$event->bg_image_path))}}) no-repeat center center fixed;
+                    background-size: cover;
+                }
+            </style>
+        @endif
 
-        {{-- Push for sticky footer--}}
-        @stack('footer')
-    </div>
+    </head>
+    <body class="attendize">
+        <div id="event_page_wrap" vocab="http://schema.org/" typeof="Event">
+            @yield('content')
 
-    {{-- Sticky Footer--}}
-    @yield('footer')
+            {{-- Push for sticky footer--}}
+            @stack('footer')
+        </div>
 
-    <a href="#intro" style="display:none;" class="totop"><i class="ico-angle-up"></i>
-        <span style="font-size:11px;">@lang("basic.TOP")</span></a>
+        {{-- Sticky Footer--}}
+        @yield('footer')
 
-    @include("Shared.Partials.LangScript")
-    {!!Html::script(config('attendize.cdn_url_static_assets').'/assets/javascript/frontend.js')!!}
+        <a href="#intro" style="display:none;" class="totop"><i class="ico-angle-up"></i>
+            <span style="font-size:11px;">@lang("basic.TOP")</span></a>
+
+        @include("Shared.Partials.LangScript")
+        {!!Html::script(config('attendize.cdn_url_static_assets').'/assets/javascript/frontend.js')!!}
 
 
-    @if(isset($secondsToExpire))
-    <script>
-        if ($('#countdown')) {
-            setCountdown($('#countdown'), 
-            {{$secondsToExpire}});
-        }
-    </script>
-    @endif
+        @if(isset($secondsToExpire))
+        <script>if($('#countdown')) {setCountdown($('#countdown'), {{$secondsToExpire}});}</script>
+        @endif
 
-    @include('Shared.Partials.GlobalFooterJS')
-</body>
-
+        @include('Shared.Partials.GlobalFooterJS')
+    </body>
 </html>
