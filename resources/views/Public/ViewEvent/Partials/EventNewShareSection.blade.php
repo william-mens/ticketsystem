95d63ce939150d621310adab2ae23000e21c4e28
William Andoh-Mensah <menswill149@gmail.com>
2024-10-09 11:22:13 +0000

    transform

--- resources/views/Public/ViewEvent/Partials/EventNewShareSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventNewShareSection.blade.php
@@ -10,9 +10,9 @@
             <ul class="rrssb-buttons clearfix ">
 
                 @if($event->social_show_facebook)
-                <li class="rrssb-facebook" style="height:20px;">
+                <li class="rrssb-facebook" style="height:25px;">
                     <!-- https://www.facebook.com/sharer/sharer.php?u={{$event->event_url}} -->
-                    <a href="#" class="popup">
+                    <a href="#" class="popup" style="display:flex; justify-content:center;">
                         <span class="rrssb-icon">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="28px" height="28px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve">
                                 <path d="M27.825,4.783c0-2.427-2.182-4.608-4.608-4.608H4.783c-2.422,0-4.608,2.182-4.608,4.608v18.434
@@ -25,9 +25,9 @@
                 </li>
                 @endif
                 @if($event->social_show_linkedin)
-                <li class="rrssb-linkedin" style="height:20px;">
+                <li class="rrssb-linkedin" style="height:25px;">
                     <!-- http://www.linkedin.com/shareArticle?mini=true&amp;url={{$event->event_url}}?title={{urlencode($event->title)}}&amp;summary={{{Str::words(md_to_str($event->description), 20)}}} -->
-                    <a href="#" class="popup">
+                    <a href="#" class="popup" style="display:flex; justify-content:center;">
                         <span class="rrssb-icon">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="28px" height="28px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve">
                                 <path d="M25.424,15.887v8.447h-4.896v-7.882c0-1.979-0.709-3.331-2.48-3.331c-1.354,0-2.158,0.911-2.514,1.803
@@ -42,9 +42,9 @@
                 </li>
                 @endif
                 @if($event->social_show_twitter)
-                <li class="rrssb-twitter" style="height:20px;">
+                <li class="rrssb-twitter" style="height:25px;">
                     <!-- http://twitter.com/intent/tweet?text=Check out: {{$event->event_url}} {{{Str::words(md_to_str($event->description), 20)}}} -->
-                    <a href="#" class="popup">
+                    <a href="#" class="popup" style="display:flex; justify-content:center;">
                         <span class="rrssb-icon">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                                 width="28px" height="28px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve">
@@ -62,8 +62,8 @@
                 @endif
                 @if($event->social_show_whatsapp)
                 <!-- whatsapp://send?text={{urlencode($event->title . ' - ' . $event->event_url)}} -->
-                <li class="rrssb-whatsapp" style="height:20px;">
-                    <a style="background-color: #43d854;" href="#" data-action="share/whatsapp/share">
+                <li class="rrssb-whatsapp" style="height:25px;">
+                    <a style="display:flex; justify-content:center;background-color: #43d854;" href="#" data-action="share/whatsapp/share">
                         <span class="rrssb-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" viewBox="0 0 90 90">
                                 <path d="M90 43.84c0 24.214-19.78 43.842-44.182 43.842a44.256 44.256 0 0 1-21.357-5.455L0 90l7.975-23.522a43.38 43.38 0 0 1-6.34-22.637C1.635 19.63 21.415 0 45.818 0 70.223 0 90 19.628 90 43.84zM45.818 6.983c-20.484 0-37.146 16.535-37.146 36.86 0 8.064 2.63 15.533 7.076 21.61l-4.64 13.688 14.274-4.537A37.122 37.122 0 0 0 45.82 80.7c20.48 0 37.145-16.533 37.145-36.857S66.3 6.983 45.818 6.983zm22.31 46.956c-.272-.447-.993-.717-2.075-1.254-1.084-.537-6.41-3.138-7.4-3.495-.993-.36-1.717-.54-2.438.536-.72 1.076-2.797 3.495-3.43 4.212-.632.72-1.263.81-2.347.27-1.082-.536-4.57-1.672-8.708-5.332-3.22-2.848-5.393-6.364-6.025-7.44-.63-1.076-.066-1.657.475-2.192.488-.482 1.084-1.255 1.625-1.882.543-.628.723-1.075 1.082-1.793.363-.718.182-1.345-.09-1.884-.27-.537-2.438-5.825-3.34-7.977-.902-2.15-1.803-1.793-2.436-1.793-.63 0-1.353-.09-2.075-.09-.722 0-1.896.27-2.89 1.344-.99 1.077-3.788 3.677-3.788 8.964 0 5.288 3.88 10.397 4.422 11.113.54.716 7.49 11.92 18.5 16.223 11.01 4.3 11.01 2.866 12.996 2.686 1.984-.18 6.406-2.6 7.312-5.107.9-2.513.9-4.664.63-5.112z" />
@@ -74,8 +74,8 @@
                 </li>
                 @endif
                 @if($event->social_show_email)
-                <li class="rrssb-email" style="height:20px;">
-                    <a href="mailto:?subject=Check This Out&body={{urlencode($event->event_url)}}">
+                <li class="rrssb-email" style="height:25px;">
+                    <a href="mailto:?subject=Check This Out&body={{urlencode($event->event_url)}}" style="display:flex; justify-content:center;">
                         <span class="rrssb-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="28px" height="28px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve">
                                 <g>
